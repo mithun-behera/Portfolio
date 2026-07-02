76fffe350cf87927a770ad84389fa1713b6feffc
james <beheramithun2021@gamil.com>
2026-07-02 22:44:45 +0530

    Remove Backend file

--- portfolio-backend/src/main/java/com/mithun/portfoliobackend/repository/ContactMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mithun.portfoliobackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mithun.portfoliobackend.entity.ContactMessage;
-
-public interface ContactMessageRepository extends JpaRepository<ContactMessage,Long>{
-     
-}
